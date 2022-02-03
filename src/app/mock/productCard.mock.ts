b754d3a2e55498dd3ad425c6e7372eafabb89d47
Daniil <chugunov1d@gmail.com>
2022-02-03 22:03:53 +0400

    CardDetails

--- src/app/mock/productCard.mock.ts
+++ src/app/mock/productCard.mock.ts
@@ -6,7 +6,8 @@ export const productCards: productCardModel[] = [
     img: 'https://static.gabestore.ru/product/9q2Co3nUvYtvUJ9bBu1Q12cr-WGSCGwp.jpg',
     name: 'The last Campfire',
     price: 324,
-    description: 'First',
+    description:
+      'The Last Campfire is an adventure, a story of a lost ember trapped in a puzzling place, searching for meaning and a way home.',
   },
 
   {
@@ -15,7 +16,7 @@ export const productCards: productCardModel[] = [
     name: 'Kenshi',
     price: 369,
     description:
-      'Firstddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd',
+      'Kenshi is an indie PC game developed by Lo-Fi Games. Initially released for early access in 2013, Kenshi was fully released in December of 2018. On Steam, users can collect Steam Trading Cards and purchase the Kenshi Original Soundtrack',
   },
 
   {
@@ -24,7 +25,7 @@ export const productCards: productCardModel[] = [
     name: 'Dark Souls 3',
     price: 500,
     description:
-      'Firstddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd',
+      'Dark Souls III is an action role-playing video game developed by FromSoftware and published by Bandai Namco Entertainment for PlayStation 4, Xbox One, and Microsoft Windows.',
   },
 
   {
@@ -33,42 +34,54 @@ export const productCards: productCardModel[] = [
     name: 'PLANET ZOO',
     price: 475,
     description:
-      'Firstddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd',
+      'Planet Zoo is a construction and management simulation video game developed and published by Frontier Developments for Microsoft Windows.',
   },
   {
     id: 4,
     img: 'https://static.gabestore.ru/product/bcQLUAPEQBW1aK09-WlXBlBuyNNo67eB.jpg',
     name: 'Lemnis Gate',
     price: 370,
+    description:
+      'Lemnis Gate is a time-warping, turn-based combat strategy FPS. Over five alternating turns taking place in a 25 second time-loop, you are called to master the unique abilities of a varied cast of deep-space operatives and out-skill',
   },
   {
     id: 5,
     img: 'https://static.gabestore.ru/product/1CDqljSNDcnWTBWQxW018wVIOsMjVZeR.jpg',
     name: 'Little Nightmares',
     price: 150,
+    description:
+      'Little Nightmares is a puzzle-platform horror adventure game developed by Tarsier Studios and published by Bandai Namco Entertainment',
   },
   {
     id: 6,
     img: 'https://static.gabestore.ru/product/4QJPHD0rC5liuRJV7athPC8xLP2X4TZD.jpg',
     name: 'Frostpunk',
     price: 180,
+    description:
+      'Frostpunk is a city-survival game where heat means life and every decision comes with a cost. Weight your choices. Will you allow child-labour?',
   },
   {
     id: 7,
     img: 'https://static.gabestore.ru/product/VrzfwAMQgLOF720REXZMqpwNOm9MhTQN.jpg',
     name: 'Ace Attorney',
     price: 650,
+    description:
+      'Ace Attorney is a series of adventure video game legal dramas developed by Capcom. The first entry in the series, Phoenix Wright: Ace Attorney, was released in 2001',
   },
   {
     id: 8,
     img: 'https://static.gabestore.ru/product/f0eMXPJKDCEiWyTxcSBsIoCOrPeEvEls.jpeg',
     name: 'BATTLEFIELD 2042',
     price: 3400,
+    description:
+      'Battlefield™ 2042 is a first-person shooter that marks the return to the iconic all-out warfare of the franchise. Adapt and overcome in a near-future world transformed by disorder',
   },
   {
     id: 9,
     img: 'https://static.gabestore.ru/product/rvPK78980BwZcGU4ejs_zsjR0KB-E_9n.jpg',
     name: 'Sable',
     price: 449,
+    description:
+      'The sable is a species of marten, a small omnivorous mammal primarily inhabiting the forest environments of Russia, from the Ural Mountains throughout Siberia, and northern Mongolia.',
   },
 ];
